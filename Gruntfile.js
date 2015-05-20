5f7e9d6158238ed8f23e9d37d68c137d44cd4c3b
IagoLast <iago.lastra@gmail.com>
2015-05-20 18:23:12 +0200

    Remove unused styles, add container classes

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,49 +1,43 @@
 var autoprefixer = require('autoprefixer-core');
 
 module.exports = function(grunt) {
-
-  // Project configuration.
-  grunt.initConfig({
-    sass: {
-      dist: {
-        files: {
-          'dist/fgrid.css': 'src/fgrid.scss',
-          'dist/fg_extra.css': 'src/fg_extra.scss',
-        }
-      }
-    },
-    autoprefixer: {
-      multiple_files: {
-        options: {
-          browsers: ['last 1 versions']
+    grunt.initConfig({
+        sass: {
+            dist: {
+                files: {
+                    'dist/fgrid.css': 'src/fgrid.scss',
+                }
+            }
         },
-        src: 'dist/*.css',
-      },
-    },
-    cssmin: {
-      minify: {
-        files: [{
-          expand: true,
-          cwd: 'dist',
-          src: ['*.css', '!*.min.css'],
-          dest: 'dist',
-          ext: '.min.css'
-        }]
-      }
-    },
-  });
+        autoprefixer: {
+            dist: {
+                options: {
+                    browsers: ['last 1 versions']
+                },
+                src: 'dist/*.css',
+            },
+        },
+        cssmin: {
+            minify: {
+                files: [{
+                    expand: true,
+                    cwd: 'dist',
+                    src: ['*.css', '!*.min.css'],
+                    dest: 'dist',
+                    ext: '.min.css'
+                }]
+            }
+        },
+    });
 
-  // Load Plugins
-  grunt.loadNpmTasks('grunt-contrib-sass');
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
-  grunt.loadNpmTasks('grunt-autoprefixer');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-autoprefixer');
 
-  // Default task(s).
-  grunt.registerTask('default',
-  [
-  'sass',
-  'autoprefixer',
-  'cssmin',
-  ]);
+    grunt.registerTask('default', [
+        'sass',
+        'autoprefixer',
+        'cssmin',
+    ]);
 
 };
